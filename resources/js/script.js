9159eeb21d316218412aa6df1d5ee79b77f7761f
limon4ikas <limon4ikas@gmail.com>
2020-10-05 16:03:56 +0300

    Animations done

--- resources/js/script.js
+++ resources/js/script.js
@@ -50,4 +50,41 @@ $(document).ready(function () {
     }
     });
 
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(
+        (direction) => {
+          $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+        },
+        {
+          offset: '50%',
+        }
+      );
+
+      $('.js--wp-2').waypoint(
+        (direction) => {
+          $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+        },
+        {
+          offset: '50%',
+        }
+      );
+
+      $('.js--wp-3').waypoint(
+        (direction) => {
+          $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+        },
+        {
+          offset: '50%',
+        }
+      );
+
+      $('.js--wp-4').waypoint(
+        (direction) => {
+          $('.js--wp-4').addClass('animate__animated animate__pulse');
+        },
+        {
+          offset: '50%',
+        }
+      );
+
 });
